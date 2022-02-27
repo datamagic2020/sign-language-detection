df5e3c3dd7f150696841942b08b161914c50de96
Data Magic <66531576+datamagic2020@users.noreply.github.com>
2022-02-27 23:33:22 +0530

    Add files via upload

---
+++ HandSignLangDetection.py
@@ -0,0 +1,64 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
+
+finger_tips = [8, 12, 16, 20]
+thumb_tip = 4
+
+like_img = cv2.imread("images/like.png")
+like_img = cv2.resize(like_img, (200, 180))
+
+dislike_img = cv2.imread("images/dislike.png")
+dislike_img = cv2.resize(dislike_img, (200, 180))
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    h, w, c = img.shape
+    results = hands.process(img)
+
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            lm_list = []
+            for id, lm in enumerate(hand_landmark.landmark):
+                lm_list.append(lm)
+            finger_fold_status = []
+            for tip in finger_tips:
+                x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                # print(id, ":", x, y)
+                cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                    cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+
+            print(finger_fold_status)
+
+            if all(finger_fold_status):
+                # like
+                if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y:
+                    print("LIKE")
+                    cv2.putText(img, "LIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                    h, w, c = like_img.shape
+                    img[35:h + 35, 30:w + 30] = like_img
+                # Dislike
+                if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y:
+                    cv2.putText(img, "DISLIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    print("DISLIKE")
+                    h, w, c = dislike_img.shape
+                    img[35:h + 35, 30:w + 30] = dislike_img
+
+            mp_draw.draw_landmarks(img, hand_landmark,
+                                   mp_hands.HAND_CONNECTIONS,
+                                   mp_draw.DrawingSpec((0, 0, 255), 6, 3),
+                                   mp_draw.DrawingSpec((0, 255, 0), 4, 2)
+                                   )
+
+    cv2.imshow("Hand Sign Detection", img)
+    cv2.waitKey(1)
